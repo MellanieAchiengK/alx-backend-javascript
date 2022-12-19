bcf2b578e5ae48dd969ce6b9dfef917b899fd8e6
Mellanie <mellanie.achieng@gmail.com>
2022-12-19 22:13:27 +0000

    Mandatory Tasks Added

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const temp = [];
+    for (const value of array) {
+      temp.push(appendString + value);
+    }
+  
+    return temp;
+  }
